9993f5a1867150594e065562b43906a8cd4ccc42
Nova-Noir <mail@novanoir.dev>
2023-01-29 00:40:57 +0800

    :boom: Add plugin @easyops-cn/docusaurus-search-local

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,6 +103,26 @@ const config = {
     ],
   ],
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        indexBlog: false,
+        docsRouteBasePath: ["blockchain", "pwn"],
+        language: ["en", "zh"],
+        
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
