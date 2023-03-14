e55b10f5ab4096b7afc55ff17d4366b12e8566b0
Nova-Noir <mail@novanoir.dev>
2023-03-14 19:26:17 +0800

    :Boom: Update Index Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,144 @@
 import React from 'react';
+import { useState } from 'react';
+import styles from './index.module.scss';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 
-import styles from './index.module.css';
+import TitleImg from '@site/static/img/index/menu/title.svg';
+import StarIcon from '@site/static/img/index/menu/star.svg';
+import MoonIcon from '@site/static/img/index/menu/moon.svg';
+import DogIcon from '@site/static/img/index/menu/dog.svg';
+import MountainIcon from '@site/static/img/index/menu/mountain.svg';
+import ProfileImg from '@site/static/img/index/menu/menu-1.svg';
+import SiteImg from '@site/static/img/index/menu/menu-2.svg';
+import ProductImg from '@site/static/img/index/menu/menu-3.svg';
+import MemoriesImg from '@site/static/img/index/menu/menu-4.svg';
 
-
-const HeroBanner = require("@site/static/img/skadi_banner.png").default
+const Svg = ({ Svg, link }) => {
+  return (
+    <a href={link} target='_blank'>
+      <Svg className={styles['last-chapter__sns-nav']} />
+    </a>
+  )
+}
 const svgList = [
   {
     title: 'bilibili',
     Svg: require('@site/static/img/bilibili.svg').default,
-    link: 'https://space.bilibili.com/11966801',
-    color: '#00b3ef'
+    link: 'https://space.bilibili.com/11966801'
   },
   {
     title: 'github',
     Svg: require('@site/static/img/github.svg').default,
-    link: 'https://github.com/Nova-Noir',
-  }
-]
-const buttonList = [
-  {
-    title: 'Blog',
-    to: '/blog'
-  },
-  {
-    title: 'About',
-    to: '/about'
+    link: 'https://github.com/Nova-Noir'
   }
 ]
 
-const Svg = ({ Svg, link, color=''}) => {
-  return (
-    <a href={link} target='_blank'>
-      <Svg className={styles.svg} style={{ fill: color }} />
-    </a>
-  )
-}
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+
+function AboutComponent() {
+  const [dropMenu, setDropMenu] = useState(false);
   return (
-    <div className={styles.hero}>
-      <div className={styles.leftComponent}>
-        <h1 className={styles.heroTitle}>NovaNo1r <span>メモ帳</span></h1>
-        <h1 className={styles.heroSubtitle}>――{siteConfig.tagline}</h1>
-        <p className={styles.heroContent}>
-          巨型二次元高手 💨
-          <br />
-          想写一个好看的主页但是我写了一晚上就这水平, 还在造... ❤
-        </p>
-        <div className={styles.svgContainer}>
-          {svgList.map((item, _) => {
-            return <Svg {...item} key={item.title}/>
-          })}
-        </div>
-        <div className={styles.buttonContainer}>
-          {buttonList.map((item, _) => {
-            return <Link className={styles.button} to={item.to} key={item.title}>{item.title}</Link>
-          })}
-        </div>
+      <article className={styles['last-chapter']}>
+      <div className={styles['last-chapter__bg-help']} />
+      <div className={styles['last-chapter__bg']} />
+      <div className={styles['last-chapter__ball']} />
+      <div className={styles['last-chapter__frame']}>
+        <div />
+        <div />
+        <div />
+        <div />
       </div>
-      <Link to={'/404'}></Link>
-      <div className={styles.rightComponent}>
-        <img src={HeroBanner} alt='HeroImg' />
+      <div className={styles['last-chapter__decoration']}>
+        <div className={styles['star']} />
+        <div className={styles['star']} />
+        <div className={styles['star']} />
+        <div className={styles['star']} />
+        <div className={styles['moon']} />
       </div>
-    </div>
+      <div className={styles['last-chapter__logo']} />
+      <nav className={styles['last-chapter__sns-nav']}>
+        { svgList.map((item, _) => {
+          return <Svg {...item} key={item.title}/>
+        })}
+      </nav>
+
+      <button type="button"
+              className={styles['last-chapter__menu-btn']}
+              aria-expanded={dropMenu ? "true" : "false"}
+              onClick={() => setDropMenu((prev) => !prev)}>
+        <div />
+        <div />
+        <div />
+      </button>
+      <DropMenu dropMenu={dropMenu} setDropMenu={setDropMenu}/>
+    </article>    
   );
 }
+
+
+const DropMenu = ({dropMenu, setDropMenu}) => (
+  <div className={`${styles['menu']} ${dropMenu ? styles['show'] : ""}`}>
+    <section className={styles["menu__space"]}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </section>
+    <section className={styles["menu__box"]}>
+      <div className={styles["bg"]} />
+      <TitleImg className={styles["menu-title"]} />
+      <nav className={styles["nav"]}>
+        <Link to="/index">
+          <div className={styles["inner"]}>
+            <div className={styles["icon"]}>
+              <StarIcon />
+            </div>
+            <ProfileImg className={styles["title"]} />
+          </div>
+        </Link>
+        <Link to="/blog">
+          <div className={styles["inner"]}>
+            <div className={styles["icon"]}>
+              <MountainIcon />
+            </div>
+            <SiteImg className={styles["title"]} />
+          </div>
+        </Link>
+        <Link to="/repruducting">
+          <div className={styles["inner"]}>
+            <div className={styles["icon"]}>
+              <DogIcon />
+            </div>
+            <ProductImg className={styles["title"]} />
+          </div>
+        </Link>
+        <Link to="/posts">
+          <div className={styles["inner"]}>
+            <div className={styles["icon"]}>
+              <MountainIcon />
+            </div>
+            <MemoriesImg className={styles["title"]} />
+          </div>
+        </Link>
+      </nav>
+      <div className={styles["colors"]}>
+        <div></div><div></div><div></div><div></div>
+      </div>
+    </section>
+    <button type="button"
+            className={styles["menu__close-btn"]}
+            aria-expanded={dropMenu ? "true" : "false"}
+            onClick={() => setDropMenu((prev) => !prev)}>
+      <div></div><div></div>
+    </button>
+  </div>
+)
+
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      description="blog,novanoir,novano1r,nova,ctf,pwn,博客,">
-      <main>
-        <HomepageHeader />
-      </main>
-    </Layout>
-  );
-}
+    return (
+        <main>
+          <AboutComponent />
+        </main>
+    );
+  }
