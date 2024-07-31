3821e88ffbdd4cc1b5b3578ab7dbb4ead94b071a
MuelNova <n@ova.moe>
2024-07-31 16:41:36 +0800

    :speech_balloon: Update Social Link

--- src/contents/pages/about/_Svg.tsx
+++ src/contents/pages/about/_Svg.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
 
 interface SvgProps {
-    Svg: React.FunctionComponent<React.SVGProps<SVGElement>>;
-    link: string;
-    Name?: string;
-    style?: React.CSSProperties;
+  Svg: React.FunctionComponent<React.SVGProps<SVGElement>>;
+  link: string;
+  Name?: string;
+  style?: React.CSSProperties;
 }
 
-const Svg: React.FC<SvgProps> = ({ Svg, link, Name='', style={}}) => {
-    return (
-      <a href={link} target='_blank'>
-        <Svg className={Name} style={style}/>
-      </a>
-    )
-}
+const Svg: React.FC<SvgProps> = ({ Svg, link, Name = "", style = {} }) => {
+  const defaultStyle: React.CSSProperties = {
+    top: "20%",
+    left: "20%",
+    width: "60%",
+    height: "60%",
+    position: "absolute",
+    display: "block",
+  };
 
+  style = { ...defaultStyle, ...style };
 
+  return (
+    <a href={link} target="_blank">
+      <Svg className={Name} style={style} />
+    </a>
+  );
+};
 
 export default Svg;
--- src/theme/utils/_SocialMediaList.tsx
+++ src/theme/utils/_SocialMediaList.tsx
@@ -1,19 +1,29 @@
 const svgList = [
-    {
-      title: 'bilibili',
-      Svg: require('@site/src/static/img/bilibili.svg').default,
-      link: 'https://space.bilibili.com/11966801'
-    },
-    {
-      title: 'github',
-      Svg: require('@site/src/static/img/github.svg').default,
-      link: 'https://github.com/MuelNova'
-    },
-    {
-      title: 'steam',
-      Svg: require('@site/src/static/img/steam.svg').default,
-      link: 'https://steamcommunity.com/id/muelnova'
-    }
-]
+  {
+    title: "bilibili",
+    Svg: require("@site/src/static/img/bilibili.svg").default,
+    link: "https://space.bilibili.com/11966801",
+  },
+  {
+    title: "github",
+    Svg: require("@site/src/static/img/github.svg").default,
+    link: "https://github.com/MuelNova",
+  },
+  {
+    title: "steam",
+    Svg: require("@site/src/static/img/steam.svg").default,
+    link: "https://steamcommunity.com/id/muelnova",
+  },
+  {
+    title: "Email",
+    Svg: require("@site/src/static/img/email.svg").default,
+    link: "mailto:n@ova.moe",
+  },
+  {
+    title: "rss",
+    Svg: require("@site/src/static/img/rss.svg").default,
+    link: "https://n.ova.moe/blog/rss.xml",
+  },
+];
 
 export default svgList;
