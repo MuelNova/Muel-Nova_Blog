adac6a195b818d91e49d0f7ad1e77f109692bcee
MuelNova <n@ova.moe>
2024-09-24 20:07:06 +0800

    :memo: Update FriendLinks

--- src/contents/pages/links/_linksVals.tsx
+++ src/contents/pages/links/_linksVals.tsx
@@ -4,8 +4,16 @@ const List: LinkCardProps[] = [
   {
     link: "https://note.silente.dev/",
     linkText: "Silente's Note",
-    description: "SilentE 是一个顶尖二次元",
+    description:
+      "SilentE 是区块链大师，Web3 米恰飞起来了，又会 Web 安全，太厉害了",
     icon: "https://note.silente.dev/static/icon.png",
+  },
+  {
+    link: "https://blog.hzao.top/",
+    linkText: "Hzao's Blog",
+    description:
+      "Hzao 是 Swift 大师，获得了 WWDC23 Swift Student Challenge 大奖，又去 Google 实习，太牛了",
+    icon: "https://secure.gravatar.com/avatar/d73560fa4904678be8395e90446b6ffb",
   },
 ];
 
