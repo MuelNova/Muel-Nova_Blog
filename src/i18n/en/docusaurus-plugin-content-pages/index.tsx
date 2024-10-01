bde10314a6c1a73a90ab32c2ff958dd71f05c164
MuelNova <n@ova.moe>
2024-10-01 14:10:59 +0800

    :globe_with_meridians: Update translations for Friendlinks and Index

--- src/i18n/en/docusaurus-plugin-content-pages/index.tsx
+++ src/i18n/en/docusaurus-plugin-content-pages/index.tsx
@@ -1,138 +1,193 @@
-import React from 'react';
-import { useState } from 'react';
-import styles from '@site/src/contents/pages/index.module.scss';
-import Link from '@docusaurus/Link';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import styles from "@site/src/contents/pages/index.module.scss";
+import Link from "@docusaurus/Link";
+import Translate from "@docusaurus/Translate";
 
-import TitleImg from '@site/src/static/img/index/menu/title.svg';
-import StarIcon from '@site/src/static/img/index/menu/star.svg';
-import MoonIcon from '@site/src/static/img/index/menu/moon.svg';
-import DogIcon from '@site/src/static/img/index/menu/dog.svg';
-import MountainIcon from '@site/src/static/img/index/menu/mountain.svg';
-import ProfileImg from '@site/src/static/img/index/menu/menu-1.svg';
-import SiteImg from '@site/src/static/img/index/menu/menu-2.svg';
-import ProductImg from '@site/src/static/img/index/menu/menu-3.svg';
-import MemoriesImg from '@site/src/static/img/index/menu/menu-4.svg';
-import svgList from '@site/src/theme/utils/_SocialMediaList';
-
-const Svg = ({ Svg, link }) => {
-  return (
-    <a href={link} target='_blank'>
-      <Svg className={styles['last-chapter__sns-nav']} />
-    </a>
-  )
-}
-
-
-
+import TitleImg from "@site/src/static/img/index/menu/title.svg";
+import StarIcon from "@site/src/static/img/index/menu/star.svg";
+import MoonIcon from "@site/src/static/img/index/menu/moon.svg";
+import DogIcon from "@site/src/static/img/index/menu/dog.svg";
+import MountainIcon from "@site/src/static/img/index/menu/mountain.svg";
+import AniEnabledIcon from "@site/src/static/img/index/menu/ani_enable.svg";
+import AniDisabledIcon from "@site/src/static/img/index/menu/ani_disable.svg";
+import svgList from "@site/src/theme/utils/_SocialMediaList";
+import Svg from "@site/src/contents/pages/about/_Svg";
 
 function AboutComponent() {
   const [dropMenu, setDropMenu] = useState(false);
+  const [hasPlayedAnimation, setHasPlayedAnimation] = useState(false);
+  const [disableAnimation, setDisableAnimation] = useState(false);
+
+  useEffect(() => {
+    const savedDisableAnimation =
+      localStorage.getItem("disableAnimation") === "true";
+    setDisableAnimation(savedDisableAnimation);
+
+    const showFirstAnimation = !sessionStorage.getItem("hasPlayedAnimation");
+
+    if (!savedDisableAnimation && showFirstAnimation) {
+      setHasPlayedAnimation(false);
+      sessionStorage.setItem("hasPlayedAnimation", "true");
+    } else {
+      setHasPlayedAnimation(true);
+    }
+  }, []);
+
+  const DropMenu = () => (
+    <div className={`${styles["menu"]} ${dropMenu ? styles["show"] : ""}`}>
+      <section className={styles["menu__space"]}>
+        <div />
+        <div />
+        <div />
+        <div />
+      </section>
+      <section className={styles["menu__box"]}>
+        <div className={styles["bg"]} />
+        <TitleImg className={styles["menu-title"]} />
+        <nav className={styles["nav"]}>
+          <Link to="/blog">
+            <div className={styles["inner"]}>
+              <div className={styles["icon"]}>
+                <MountainIcon />
+              </div>
+              <p className={styles["text"]}>
+                <Translate>博客</Translate>
+              </p>
+              {/* <SiteImg className={styles["title"]} /> */}
+            </div>
+          </Link>
+          <Link to="/posts">
+            <div className={styles["inner"]}>
+              <div className={styles["icon"]}>
+                <MoonIcon />
+              </div>
+              <p className={styles["text"]}>
+                <Translate>文章</Translate>
+              </p>
+              {/* <MemoriesImg className={styles["title"]} /> */}
+            </div>
+          </Link>
+          <Link to="/reproducing">
+            <div className={styles["inner"]}>
+              <div className={styles["icon"]}>
+                <DogIcon />
+              </div>
+              <p className={styles["text"]}>
+                <Translate>复现</Translate>
+              </p>
+              {/* <ProductImg className={styles["title"]} /> */}
+            </div>
+          </Link>
+          <Link to="/about">
+            <div className={styles["inner"]}>
+              <div className={styles["icon"]}>
+                <StarIcon />
+              </div>
+              <p className={styles["text"]}>
+                <Translate>关于</Translate>
+              </p>
+              {/* <ProfileImg className={styles["title"]} /> */}
+            </div>
+          </Link>
+          <Link to="/links">
+            <div className={styles["inner"]}>
+              <div className={styles["icon"]}>
+                <MountainIcon />
+              </div>
+              <p className={styles["text"]}>
+                <Translate>友链</Translate>
+              </p>
+              {/* <ProfileImg className={styles["title"]} /> */}
+            </div>
+          </Link>
+        </nav>
+        <div className={styles["colors"]}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+      <button
+        type="button"
+        className={styles["menu__close-btn"]}
+        aria-expanded={dropMenu ? "true" : "false"}
+        onClick={() => {
+          setDropMenu((prev) => !prev);
+          if (!hasPlayedAnimation) {
+            setHasPlayedAnimation(true);
+          }
+        }}
+      >
+        <div></div>
+        <div></div>
+      </button>
+    </div>
+  );
+
   return (
-      <article className={styles['last-chapter']}>
-      <div className={styles['last-chapter__bg-help']} />
-      <div className={styles['last-chapter__bg']} />
-      <div className={styles['last-chapter__ball']} />
-      <div className={styles['last-chapter__frame']}>
+    <article
+      className={`${styles["last-chapter"]} ${
+        hasPlayedAnimation ? styles["animation-done"] : styles["animation"]
+      }`}
+    >
+      <div className={styles["last-chapter__bg-help"]} />
+      <div className={styles["last-chapter__bg"]} />
+      <div className={styles["last-chapter__ball"]} />
+      <div className={styles["last-chapter__frame"]}>
         <div />
         <div />
         <div />
         <div />
       </div>
-      <div className={styles['last-chapter__decoration']}>
-        <div className={styles['star']} />
-        <div className={styles['star']} />
-        <div className={styles['star']} />
-        <div className={styles['star']} />
-        <div className={styles['moon']} />
+      <div className={styles["last-chapter__decoration"]}>
+        <div className={styles["star"]} />
+        <div className={styles["star"]} />
+        <div className={styles["star"]} />
+        <div className={styles["star"]} />
+        <div className={styles["moon"]} />
       </div>
-      <div className={styles['last-chapter__logo']} />
-      <nav className={styles['last-chapter__sns-nav']}>
-        { svgList.map((item, _) => {
-          return <Svg {...item} key={item.title}/>
+      <div className={styles["last-chapter__logo"]} />
+      <nav className={styles["last-chapter__sns-nav"]}>
+        {svgList.map((item, _) => {
+          return <Svg {...item} key={item.title} />;
         })}
       </nav>
 
-      <button type="button"
-              className={styles['last-chapter__menu-btn']}
-              aria-expanded={dropMenu ? "true" : "false"}
-              onClick={() => setDropMenu((prev) => !prev)}>
+      <button
+        className={styles["anibtn"]}
+        onClick={() => {
+          const prev = disableAnimation;
+          setDisableAnimation(!prev);
+          setHasPlayedAnimation(!prev);
+          localStorage.setItem("disableAnimation", (!prev).toString());
+        }}
+      >
+        {!disableAnimation ? <AniEnabledIcon /> : <AniDisabledIcon />}
+      </button>
+
+      <button
+        type="button"
+        className={styles["last-chapter__menu-btn"]}
+        aria-expanded={dropMenu ? "true" : "false"}
+        onClick={() => {
+          setDropMenu((prev) => !prev);
+        }}
+      >
         <div />
         <div />
         <div />
       </button>
-      <DropMenu dropMenu={dropMenu} setDropMenu={setDropMenu}/>
-    </article>    
+      <DropMenu />
+    </article>
   );
 }
 
-
-const DropMenu = ({dropMenu, setDropMenu}) => (
-  <div className={`${styles['menu']} ${dropMenu ? styles['show'] : ""}`}>
-    <section className={styles["menu__space"]}>
-      <div />
-      <div />
-      <div />
-      <div />
-    </section>
-    <section className={styles["menu__box"]}>
-      <div className={styles["bg"]} />
-      <TitleImg className={styles["menu-title"]} />
-      <nav className={styles["nav"]}>
-        <Link to="/blog">
-          <div className={styles["inner"]}>
-            <div className={styles["icon"]}>
-              <MountainIcon />
-            </div>
-            <p className={styles["text"]}>Blog</p>
-            {/* <SiteImg className={styles["title"]} /> */}
-          </div>
-        </Link>
-        <Link to="/posts">
-          <div className={styles["inner"]}>
-            <div className={styles["icon"]}>
-              <MoonIcon />
-            </div>
-            <p className={styles["text"]}>Posts</p>
-            {/* <MemoriesImg className={styles["title"]} /> */}
-          </div>
-        </Link>
-        <Link to="/reproducing">
-          <div className={styles["inner"]}>
-            <div className={styles["icon"]}>
-              <DogIcon />
-            </div>
-            <p className={styles["text"]}>Reproducing</p>
-            {/* <ProductImg className={styles["title"]} /> */}
-          </div>
-        </Link>
-        <Link to="/about">
-          <div className={styles["inner"]}>
-            <div className={styles["icon"]}>
-              <StarIcon />
-            </div>
-            <p className={styles["text"]}>About</p>
-            {/* <ProfileImg className={styles["title"]} /> */}
-          </div>
-        </Link>
-      </nav>
-      <div className={styles["colors"]}>
-        <div></div><div></div><div></div><div></div>
-      </div>
-    </section>
-    <button type="button"
-            className={styles["menu__close-btn"]}
-            aria-expanded={dropMenu ? "true" : "false"}
-            onClick={() => setDropMenu((prev) => !prev)}>
-      <div></div><div></div>
-    </button>
-  </div>
-)
-
-
 export default function Home(): JSX.Element {
-    return (
-        <main>
-          <AboutComponent />
-        </main>
-    );
-  }
+  return (
+    <main>
+      <AboutComponent />
+    </main>
+  );
+}
--- src/i18n/en/docusaurus-plugin-content-pages/links/_linksVals.tsx
+++ src/i18n/en/docusaurus-plugin-content-pages/links/_linksVals.tsx
@@ -5,14 +5,14 @@ const List: LinkCardProps[] = [
     link: "https://note.silente.dev/",
     linkText: "Silente's Note",
     description:
-      "SilentE 是区块链大师，Web3 米恰飞起来了，又会 Web 安全，太厉害了",
+      "SilentE, a blockchain master, Web3 is flying, and he knows Web security, too awesome",
     icon: "https://note.silente.dev/static/icon.png",
   },
   {
     link: "https://blog.hzao.top/",
     linkText: "Hzao's Blog",
     description:
-      "Hzao 是 Swift 大师，获得了 WWDC23 Swift Student Challenge 大奖，又去 Google 实习，太牛了",
+      "Hzao, a Swift master, won the WWDC23 Swift Student Challenge, went to Google for an internship, shout out to him",
     icon: "https://secure.gravatar.com/avatar/d73560fa4904678be8395e90446b6ffb",
   },
 ];
