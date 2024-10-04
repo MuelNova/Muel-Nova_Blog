8c30c4ad69145158044cd0c53eac539b093e6092
MuelNova <n@ova.moe>
2024-10-05 00:34:30 +0800

    :busts_in_silhouette: Add Friendship Link of @junyu33

--- src/contents/pages/links/_linksVals.tsx
+++ src/contents/pages/links/_linksVals.tsx
@@ -15,6 +15,13 @@ const List: LinkCardProps[] = [
       "Hzao 是 Swift 大师，获得了 WWDC23 Swift Student Challenge 大奖，又去 Google 实习，太牛了",
     icon: "https://secure.gravatar.com/avatar/d73560fa4904678be8395e90446b6ffb",
   },
+  {
+    link: "https://blog.junyu33.me/",
+    linkText: "Junyu's Blog",
+    description:
+      "Junyu 是川大的密码学大师，曾经和我一起在 NUS 进行交流学习，PWN 水平也很高，实在是厉害。",
+    icon: "https://junyu33.me/img/avatar.jpg",
+  },
 ];
 
 export default List;
--- src/i18n/en/docusaurus-plugin-content-pages/links/_linksVals.tsx
+++ src/i18n/en/docusaurus-plugin-content-pages/links/_linksVals.tsx
@@ -15,6 +15,13 @@ const List: LinkCardProps[] = [
       "Hzao, a Swift master, won the WWDC23 Swift Student Challenge, went to Google for an internship, shout out to him",
     icon: "https://secure.gravatar.com/avatar/d73560fa4904678be8395e90446b6ffb",
   },
+  {
+    link: "https://blog.junyu33.me/",
+    linkText: "Junyu's Blog",
+    description:
+      "Junyu, a cryptography master from Sichuan University, once exchanged and studied with me at NUS, and his PWN level is also very high, he is really amazing.",
+    icon: "https://junyu33.me/img/avatar.jpg",
+  },
 ];
 
 export default List;
